ea5ca60b1559637d812738fc8a97d1dc26237c8d
Adrien <ador@usc.edu>
2022-06-02 11:39:32 +0200

    Added my project

---
+++ main.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+
+with open('test.html','r') as html_file:
+    content=html_file.read()
+
+    soup=BeautifulSoup(content, 'lxml')
+    for link in soup.find_all('a'):
+
+        print(link.get('href'))
