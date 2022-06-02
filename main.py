3234bc32546aba014988d82906b7121d3066461c
adidor <ador@usc.edu>
2022-06-02 14:39:35 +0200

    test123

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
 from bs4 import BeautifulSoup
+import requests
+import random
 
-with open('test.html','r') as html_file:
-    content=html_file.read()
+user_input = input("Choose action (a) or comedy (c): ")
+if user_input=='c':
+    html=requests.get('https://www.justwatch.com/us/provider/netflix/movies?genres=cmy&rating_imdb=8')
+if user_input=='a':
+    html=requests.get('https://www.justwatch.com/us/provider/netflix/movies?genres=act&rating_imdb=8')
 
-    soup=BeautifulSoup(content, 'lxml')
-    for link in soup.find_all('a'):
 
-        print(link.get('href'))
-        print(link)
-        print(link)
+soup=BeautifulSoup(html.text, 'lxml')
+#soup=soup.find_all('div',class_='title-list-grid__item')
+#movie= movie.find('a').href
+#print(movie)
+movie_urls=[]
+for a in soup.find_all('a', href=True):
+    if '/us/movie/' in a['href']:
+        movie_urls.append(a['href'])
+        print("URL:", a['href'])
 
-        
+movie_url_short=random.choice(movie_urls)
+movie_url_long='https://www.justwatch.com'+ movie_url_short
+movie_html=requests.get(movie_url_long)
+soup_2=BeautifulSoup(movie_html.text,'lxml')
+
+class info:
+  def __init__(self, name, date, imdb_rating):
+    self.name = name
+    self.date = date
+    self.imdb_rating = imdb_rating
+
+movie_info=info('','','')
+
+name = soup_2.find('h1').text
+movie_info.name=name
+
+print(movie_info.name)
+
+
+
+
+
+
 
